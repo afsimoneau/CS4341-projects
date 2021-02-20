e3df26899b92e5069ae41b146bd90a9e3842fd7e
Alexander Simoneau <afsimoneau@gmail.com>
2021-02-19 20:54:52 -0500

    moving in the right direction

--- ConnectN/run.py
+++ ConnectN/run.py
@@ -12,7 +12,7 @@ random.seed(1)
 g = game.Game(7, # width
               6, # height
               4, # tokens in a row to win
-              agent.RandomAgent("random1"),       # player 1
+              aba.THE_AGENT,       # player 1
               agent.RandomAgent("random2"))       # player 2
 
 #
