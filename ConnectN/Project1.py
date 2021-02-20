6f3a2d30cb6d3d2ee49fef96d4682307a46ce603
mehalley <62576991+mehalley@users.noreply.github.com>
2021-02-19 19:05:40 -0500

    Add files via upload

---
+++ ConnectN/Project1.py
@@ -0,0 +1,70 @@
+#Project 1
+#Madeline Halley
+
+import math
+import agent
+
+###########################
+# Alpha-Beta Search Agent #
+###########################
+
+class AlphaBetaAgent(agent.Agent):
+    """Agent that uses alpha-beta search"""
+
+    # Class constructor.
+    #
+    # PARAM [string] name:      the name of this player
+    # PARAM [int]    max_depth: the maximum search depth
+    def __init__(self, name, max_depth):
+        super().__init__(name)
+        # Max search depth
+        self.max_depth = max_depth
+
+    # Pick a column.
+    #
+    # PARAM [board.Board] brd: the current board state
+    # RETURN [int]: the column where the token must be added
+    #
+    # NOTE: make sure the column is legal, or you'll lose the game.
+    def go(self, brd):
+        """Search for the best move (choice of column for the token)"""
+        # Your code here
+
+        #evaluate move and find the next best moves for the piece to win
+        #Use get_successors
+        #self.get_successors(brd)
+        #store these moves (transposition table)
+        #find the min and max depths in order to find the next best move
+
+    def boardStates():
+        #min depth -> positive infinity
+	#max depth -> negative infinity
+
+    
+
+    # Get the successors of the given board.
+    #
+    # PARAM [board.Board] brd: the board state
+    # RETURN [list of (board.Board, int)]: a list of the successor boards,
+    #                                      along with the column where the last
+    #                                      token was added in it
+    def get_successors(self, brd):
+        """Returns the reachable boards from the given board brd. The return value is a tuple (new board state, column number where last token was added)."""
+        # Get possible actions
+        freecols = brd.free_cols()
+        # Are there legal actions left?
+        if not freecols:
+            return []
+        # Make a list of the new boards along with the corresponding actions
+        succ = []
+        for col in freecols:
+            # Clone the original board
+            nb = brd.copy()
+            # Add a token to the new board
+            # (This internally changes nb.player, check the method definition!)
+            nb.add_token(col)
+            # Add board to list of successors
+            succ.append((nb,col))
+        return succ
+
+THE_AGENT = AlphaBetaAgent("Group21", 4)
