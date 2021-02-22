e4a12ae0f27685557a4bfb48db8b3b169cfe867d
Alexander Simoneau <afsimoneau@gmail.com>
2021-02-21 23:56:01 -0500

    stressed

--- ConnectN/alpha_beta_agent.py
+++ ConnectN/alpha_beta_agent.py
@@ -35,45 +35,36 @@ class AlphaBetaAgent(agent.Agent):
     def go(self, brd):
         if self.zb_table == None or (len(self.zb_table) < brd.h) or (len(self.zb_table[0]) < brd.w):
             self.zobrist_init(brd)
-            # print(self.zb_table)
-        """Search for the best move (choice of column for the token)"""
-        successors = self.get_successors(brd)
-        # print(successors)
-        best = -math.inf
-        best_index = -1
-        alpha = -math.inf
-        beta = math.inf
-        for i, succ in enumerate(successors):
-            result = self.solve(succ[0], alpha, beta, self.max_depth)
-            if result > best:
-                best_index = i
-                best = result
 
-        return successors[best_index][1]  # returns column number
+        alpha = -sys.maxsize
+        beta = sys.maxsize
+        result = self.solve(brd, alpha, beta, self.max_depth,1)
+        return result  # returns column number
         # brd.print_it()
 
     # solve for the best possible next move
-    def solve(self, brd, alpha, beta, depth):
+    def solve(self, brd, alpha, beta, depth, player):
         if depth == 0:
             return self.evaluate(brd)
 
-        #brd.print_it()
-
-        #print(f"\n{alpha}, {beta}")
         successors = self.get_successors(brd)
 
         # if there is a draw
         if len(successors) == 0:
             return 0
 
-        # If current player can win in the next move
-        for s in successors:
+        # If current player can win with specific move
+        for i, s in enumerate(successors):
             if s[0].get_outcome() != 0:
-                # 10^n is the winning ticket!! (do -num moves to find quicker wins)
-                return (int(math.pow(10, brd.n)) + 1 - self.num_moves(s[0]))
+                if self.max_depth == depth:
+                    return i
+                else:
+                    # 10^n is the winning potential (do -num moves to find quicker wins)
+                    return (int(math.pow(10, brd.n)) - (self.num_moves(s[0])+1))
+        
 
         # upper bound of score - cannot win right away
-        max = (int(math.pow(10, brd.n)) - self.num_moves(s[0]))
+        max = (int(math.pow(10, brd.n)) - self.num_moves(brd))
         curHash = self.zb_hash(brd)
         if curHash in self.trans_table:
             max = self.trans_table[curHash] + beta - 1
@@ -86,19 +77,28 @@ class AlphaBetaAgent(agent.Agent):
                 return beta
 
         # find score of all possible next moves and keep the best one
-        for s in successors:
-            score = -self.solve(s[0], -beta, -alpha, depth-1)
+        best_index = -1
+        for i, s in enumerate(successors):
+            score = -self.solve(s[0], -beta, -alpha, depth-1, 2 if (player==1) else 1)
 
             # prune if we found a better move than before
             if score >= beta:
-                return score
+                if self.max_depth == depth:
+                    return i
+                else:
+                    return score
+
             # reduce the alpha,beta window for search
             if score > alpha:
                 alpha = score
+                best_index = i
 
-        # return best
+        # return best or index of best successor
         self.trans_table[self.zb_hash(brd)] = alpha
-        return alpha
+        if self.max_depth == depth:
+            return best_index
+        else:
+            return alpha
 
     def num_moves(self, brd):
         n = 0
@@ -107,21 +107,6 @@ class AlphaBetaAgent(agent.Agent):
                 if brd.board[y][x] != 0:
                     n += 1
         return n
-
-    def zobrist_init(self, brd):
-        self.zb_table = [[(os.urandom(8), os.urandom(8))]
-                         * brd.w for i in range(brd.h)]
-        self.trans_table = {}
-
-    def zb_hash(self, brd):
-        h = 0
-        for y in range(0, brd.h):
-            for x in range(0, brd.w):
-                piece = brd.board[y][x]
-                if piece != 0:
-                    h = h ^ int.from_bytes(
-                        self.zb_table[y][x][piece-1], "little")
-        return h
 
     def find_line(self, brd, x, y, dx, dy):
         if ((x + (brd.n-1) * dx >= brd.w) or
@@ -138,6 +123,8 @@ class AlphaBetaAgent(agent.Agent):
         return (player_token, coords)
 
     def evaluate(self, brd):
+        if brd.get_outcome()!=0:
+            return (int(math.pow(10, brd.n)) + 1 - self.num_moves(brd))
         all_lines = []
         for x in range(brd.w):
             for y in range(brd.h):
@@ -147,8 +134,8 @@ class AlphaBetaAgent(agent.Agent):
                         # if we've evaluated this x,y before, ignore it. part of line(s) already calculated
                         checked = True
                 if not checked:
-                    #check directions
-                    #ignore anything out of bounds
+                    # check directions
+                    # ignore anything out of bounds
                     line = self.find_line(brd, x, y, 1, 0)  # Horizontal
                     if line[0] != 0 and len(line[1]) != 1:
                         all_lines.append(line)
@@ -171,10 +158,24 @@ class AlphaBetaAgent(agent.Agent):
             else:
                 p2_val += n
         ret = (p1_val-p2_val)  # what we will return
-        if self.max_depth % 2 == 0:
-            ret *= -1 #if max depth is even, we invert result. p2 should be at a positive value when p2 is winning
+        if self.max_depth % 2 == 1:
+            ret *= -1  # if max depth is even, we invert result. p2 should be at a positive value when p2 is winning
         return ret
 
+    def zobrist_init(self, brd):
+        self.zb_table = [[(os.urandom(8), os.urandom(8))]
+                         * brd.w for i in range(brd.h)]
+        self.trans_table = {}
+
+    def zb_hash(self, brd):
+        h = 0
+        for y in range(0, brd.h):
+            for x in range(0, brd.w):
+                piece = brd.board[y][x]
+                if piece != 0:
+                    h = h ^ int.from_bytes(
+                        self.zb_table[y][x][piece-1], "little")
+        return h
     '''
 \    
  \   
@@ -225,5 +226,4 @@ class AlphaBetaAgent(agent.Agent):
             succ.append((nb, col))
         return succ
 
-
-THE_AGENT = AlphaBetaAgent("Group21", 10)
+THE_AGENT = AlphaBetaAgent("Group21", 4)
