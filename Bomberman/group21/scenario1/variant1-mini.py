a655194bd85af5b233febbc6560808a3fbeefef1
Alexander Simoneau <afsimoneau@gmail.com>
2021-03-16 20:04:42 -0400

    todo's and general ideas for q approx. need more features?

---
+++ Bomberman/group21/scenario1/variant1-mini.py
@@ -0,0 +1,44 @@
+# This is necessary to find the main code
+import sys
+sys.path.insert(0, '../../bomberman')
+sys.path.insert(1, '..')
+
+# Import necessary stuff
+from game import Game
+from qcharacter import QCharacter
+
+# TODO This is your code!
+sys.path.insert(1, '../groupNN')
+
+# Uncomment this if you want the empty test character
+#from testcharacter import TestCharacter
+
+# Uncomment this if you want the interactive character
+# from interactivecharacter import InteractiveCharacter
+
+# Create the game
+g = Game.fromfile('v1-minimap.txt')
+
+# TODO Add your character
+
+# Uncomment this if you want the test character
+# g.add_character(TestCharacter("me", # name
+#                               "C",  # avatar
+#                               0, 0  # position
+# ))
+
+# Uncomment this if you want the interactive character
+# g.add_character(InteractiveCharacter("me", # name
+#                                      "C",  # avatar
+#                                      0, 0  # position
+# ))
+
+g.add_character(QCharacter("ai","Q",0,0))
+
+# Run!
+
+# Use this if you want to press ENTER to continue at each step
+# g.go(0)
+
+# Use this if you want to proceed automatically
+g.go(1)
